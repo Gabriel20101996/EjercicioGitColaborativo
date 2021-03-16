4fc7483894ca6d86e18d1cf3736a5a435a51d700
Gabriel Canda <gabriel.candagcb9@gmail.com>
2021-03-16 08:58:19 +0100

    Creacion del main del juego

---
+++ src/JuegoColaborativo/HistoriaJuegoColaborativa.java
@@ -0,0 +1,42 @@
+package JuegoColaborativo;
+import java.util.Scanner;
+public class HistoriaJuegoColaborativa {
+
+	static Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bienvenido a la historia donde tu eres el protagonista.");
+		System.out.println("");
+
+		System.out.println("Podras elegir entre varias opciones para decidir el rumbo de tu aventura.");
+		System.out.println("");
+
+		System.out.println("La historia comienza a las 9 de la maniana del 15 de diciembre de 1995 en el hospital Infanta Sofia de Madrid, "
+				+ "hacia un frio helador y aun no habia salido el sol.");
+		System.out.println("Hoy era un dia especial, Ingrid Baker estaba a punto de dar a luz. El medico anteriormente le habia avisado que iba a tener dos mellizos, un ninio y una ninia.");
+		System.out.println("");
+
+		System.out.println("Debido a esta situacion, el padre, Richard Williams, decidio dejar a Ingrid y cortar el contacto con ella y sus futuros hijos.");
+		System.out.println("");
+
+		System.out.println("El embarazo iba muy bien, pero iba a suceder un imprevisto desafortunado que cambiaria drasticamente el devenir de la historia, "
+				+ "y eres tu quien debe decidir que rumbo tomar. Este dia marcara tu vida y a partir de ahi depende de ti y de tus actos en quien tu decidas ser.");
+		System.out.println("");
+
+		System.out.println("Llega el momento de tomar tu primera decision. Estas en la una encrucijada antes de nacer.");
+		System.out.println("");
+		System.out.println("Decides:");
+		System.out.println("1.- Ingrid sobreviva al parto pero eso involucraria que tu hermano fallezca.");
+		System.out.println("2.- Decidir salvar a tu hermano, tendra como consecuencia la muerte de Ingrid.");
+		int eleccion = scanner.nextInt();
+
+		if (eleccion == 1){
+
+			
+		} else {
+
+			
+		}
+	}
+
+}
