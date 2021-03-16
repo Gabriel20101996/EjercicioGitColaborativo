7396447e40a0fdbbd3a50e5afea2f36a9fe870e8
Sergio GMQ <sergiogilga@gmail.com>
2021-03-16 16:52:11 +0100

    Parte 2 opciones

--- src/JuegoColaborativo/HistoriaJuegoColaborativa.java
+++ src/JuegoColaborativo/HistoriaJuegoColaborativa.java
@@ -38,5 +38,45 @@ public class HistoriaJuegoColaborativa {
 			
 		}
 	}
-//Prueba Sergio Gil
+
+	static void opcion9() {
+
+		System.out.println("Decides salir a buscar a Aiden. Tras seguir sus pisadas por el bosque, le encuentras.");
+		System.out.println("");
+		System.out.println("Comenzais a dar un paseo y a hablar sobre lo que ha ocurrido.");
+		System.out.println("En mitad de la charla, cerca de un precipicio, una manada de ciervos os empiezan a acorralar.");
+		System.out.println("Aiden se tropieza y se queda colgando de una rama.");
+		System.out.println("");
+		System.out.println("Decides:");
+		System.out.println("1.- Ayudarle");
+		System.out.println("2.- No ayudarle");
+
+		int eleccionBosque = scanner.nextInt();
+
+		if (eleccionBosque == 1) {
+			opcion11();
+		} else {
+			opcion12();
+		}
+	}
+
+	static void opcion11() {
+
+		System.out.println("Decides ir a ayudarle, la rama en la que estaba sujeto Aiden se rompe, provocando que os caigais los dos por el precipicio.");
+		System.out.println("Tras la caida, Aiden tiene un muerte instantanea y tu sobrevives.");
+	}
+
+	static void opcion12() {
+
+		System.out.println("Decides no ayudar a tu hermano, por tanto tu hermano muere.");
+		System.out.println("Tras su caida, los ciervos te empiezan a acorralar mas y terminas cayendo, provocando tu muerte tambien.");
+	}
+
+	static void opcion10() {
+
+		System.out.println("Decides esperar que vuelva tu hermano. ");
+		System.out.println("Al siguiente dia la policia acude al orfanato para comunicarte una terrible noticia, tu hermano a fallecido tras caerse por un barranco.");
+		System.out.println("");
+	}
+
 }
